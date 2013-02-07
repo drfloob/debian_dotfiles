ff0fff0a49651386778722340a52b04df478f54c
aj heller <aj@drfloob.com>
2013-02-07 12:43:54 -0800

    shell is now urxvt by default; fix xmonad config a bit

--- configs/.xmonad/xmonad.hs
+++ configs/.xmonad/xmonad.hs
@@ -8,41 +8,25 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace
 import XMonad.Util.EZConfig  
 
-myWorkspaces = ["1:web", "2:porn", "3:emacs", "4:terms", "5:ext"]
-
--- myLayout = onWorkspace "1:web" (TwoPane (0.80) (3/100)) -- $ smartBorders (tiled ||| Mirror tiled ||| Full)
---   where
---     tiled = Tall 1 0.05 ratio
---     ratio =  toRational (2/(1+sqrt(5)::Double)) -- golden ratio
-
-myManageHook = composeAll [
-  className =? "Emacs" --> doShift "3:emacs"
-  , className =? "Pidgin" --> doShift "1:web"
-  , className =? "Gimp" --> doFloat
-  ]
+myWorkspaces = ["1:web", "2:IM", "3:emacs", "4:terms", "5:docs", "6", "7", "8", "9"]
 
 main = xmonad $ defaultConfig
         { 
           modMask = mod4Mask -- Use Super instead of Alt
-        , terminal = "aterm"
+        , terminal = "urxvt"
         , workspaces = myWorkspaces
-        , manageHook = manageHook defaultConfig <+> manageSpawn <+> manageDocks
-        -- , layoutHook = avoidStruts $ myLayout $ layoutHook defaultConfig
+        , manageHook = manageHook defaultConfig
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         , startupHook = do
             spawn "xset r rate 150 50; synclient MaxTapTime=0"
             spawn "xmobar"
             spawn ". ~/.fehbg"
-            -- spawnOn "1:web" "chromium-browser gmail.com reader.google.com& pidgin"
-            -- spawnOn "2:porn" "chromium-browser --incognito reddit.com"
-            -- spawnOn "3:emacs" "emacs"
-            -- spawnOn "4:terms" "x-terminal-emulator"
         } `additionalKeys` 
         [((mod4Mask, xK_BackSpace), spawn "emacsclient -n ~/.xmonad/xmonad.hs")
         , ((mod4Mask, xK_n), spawn "chromium-browser --incognito --disable-accelerated-compositing")
         , ((mod4Mask, xK_i), spawn "pidgin")
         , ((mod4Mask, xK_t), spawn "toggle_redshift.sh")
-        , ((mod4Mask, xK_q), spawn "toggle_qwerty.sh")
+        , ((mod4Mask + mod1Mask, xK_q), spawn "toggle_qwerty.sh")
         , ((mod4Mask, xK_d), spawn "kill_powersave.sh")
         , ((0, 0x1008ff12), spawn "myaudioproxy.sh mute")
         , ((0, 0x1008ff12), spawn "myaudioproxy.sh mute")
